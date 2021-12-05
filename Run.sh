406d8bc7ddd7bc41c533e0ed5c9b004c92cc619c
1337XQ <95542986+1337XQ@users.noreply.github.com>
2021-12-05 10:33:05 +0700

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+clear
+echo -e "\e[4;31m MrHacker**** \e[0m"
+echo -e "\e[1;32m  ◡̈ \e[0m"
+echo -e "\e[1;34m"
+figlet -f slant ──────
+echo "รหัสเข้า ... "
+read a10
+if [[ -s update.shield.yemen ]];then
+echo "พบความต้องการทั้งหมด...."
+else
+echo 'Installing....'
+echo .
+echo .
+apt install figlet toilet python curl ruby -y
+apt install python3-pip
+gem install lolcat
+echo ..
+echo ...
+echo ....
+echo .....
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet -f slant MrHacker***| lolcat
+echo -e "\e[1;34m  Bypass \e[1;32m"  #blue Color
+echo -e "\e[4;34m ..... \e[0m" #underline+blue
+echo -e "\e[1;34m ......\e[0m"
+echo -e "\e[1;32m ....... \e[0m" #yellow
+echo -e "\e[1;34m ........ \e[0m"
+echo " "
+echo -e "\e[4;31m เลือกฟังก์ชั่นแล้วเติมหมายเลข... \e[0m" #red
+echo " "
+echo "Enter 1 To  Run X-SMS == เรียกใช้ X-SMS"
+echo "Enter 2 To  Track X-SMS == ติดตาม X-SMS"  #white
+echo "Enter 3 To  Update == อัปเดต"
+echo "Enter 4 To  Check Features == ตรวจสอบคุณสมบัติ "
+echo "Enter 5 To  Exit - ออก"
+
+read -p  "     หมายเลข :> " menu
+if [ $menu = 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 fakesms.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $menu = 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Track X-SMS'> call.xxx
+python3 fakesms.py track
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $menu = 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m กำลังขออัปเดตจากแหล่งที่มา..."
+echo -e "\e[1;34m ขอความพยายามสำเร็จ..."
+echo -e "\e[1;34m กำลังอัปเดตตอนนี้..."
+git clone https://github.com/X-HACKRAWI/FreeSms.git
+if [[ -s FreeSms/Run.sh ]];then
+cd FreeSms
+cp -r -f * .. > temp
+cd ..
+rm -rf  FreeSms>> temp
+rm update.shield.yemen>> temp
+rm temp
+chmod +x Run.sh
+fi
+echo -e "\e[1;32m X-SMS จะรีเฟรชทันที..."
+echo -e "\e[1;32m  ติดตั้งแพ็คเกจที่จำเป็นทั้งหมดแล้ว..."
+echo -e "\e[1;34m กด Enter เพื่อรีเฟรช..."
+read a6
+./Run.sh
+exit
+echo " "
+echo " "
+elif [ $menu = 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet -f slant X-SMS| lolcat
+echo -e "\e[1;34m MrHacker*** \e[1;34m"  #Blue Color
+toilet -f mono12 -F border read | lolcat
+echo  " "
+echo -e "\e[1;32m                   คุณสมบัติ\e[1;34m"
+echo "  การส่งที่รวดเร็วเป็นพิเศษ"
+echo "  ส่งต่างประเทศ"
+echo "  การติดตาม "
+echo "  การอัปเดตในอนาคตอัตโนมัติ"
+echo "  ใช้งานง่ายและฝังในโค้ด"
+echo ""
+echo ""
+echo -e "\e[1;31m นี้เป็นเพียงเพื่อการศึกษาหรือเพื่อเล่นตลก\e[0m"
+echo -e "\e[1;31m อย่าใช้สิ่งนี้เพื่อทำให้ผู้อื่นระคายเคือง \e[0m"
+echo -e "\e[1;31m อย่าใช้สิ่งนี้เพื่อทำร้ายผู้อื่น \e[0m"
+echo -e "\e[1;31m เราไม่รับผิดชอบต่อการใช้สคริปต์ในทางที่ผิด \e[0m"
+echo -e "\e[1;32m อัปเดตหากไม่ได้ผล\e[0m"
+echo  " "
+echo "กด Enter เพื่อกลับไปยังเมนูหลัก"
+read a3
+clear
+elif [ $menu = 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet -f slant X-SMS| lolcat
+echo -e "\e[1;34m MrHacker*** \e[1;32m"
+toilet -f sgga -F border MrHacker*** | lolcat
+echo -e "\e[1;34m ขอบคุณ !!!\e[0m"
+echo -e "\e[1;32m  ◡̈ \e[0m" #yellow
+echo -e "\e[1;34m ... \e[0m"
+echo " "
+exit 0
+else
+echo -e "\e[4;32m ตรวจพบอินพุตที่ไม่ถูกต้อง !!! \e[0m"
+echo "กด Enter เพื่อกลับไปยังเมนูหลัก"
+read a3
+clear
+fi
+done
---
+++ fakesms.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+import threading
+import string
+import base64
+import urllib.request
+import urllib.parse
+import os
+import time
+import sys
+import random
+
+try:
+    import requests
+except ImportError:
+    print('ข้อผิดพลาด !! : ไม่ได้ติดตั้งการพึ่งพาบางรายการ')
+    print("เกิดข้อผิดพลาดในการนำเข้าไลบรารี 'คำขอ'\ntodo : python3 -m pip install การร้องขอ")
+    exit()
+
+# colors
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+W = '\033[0m'
+colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m']
+
+
+
+def clr():
+	if os.name == 'nt':
+		os.system('cls')
+	else:
+		os.system('clear')
+
+def banner():
+    clr()
+    logo = """                                                  
+\033[96m
+    \033[91mMrHacker   \033[95m Hello      \033[96mv1.0   
+\033[96m  
+                                         """
+    print(logo)
+    print("\n")
+    
+
+def Track() :
+  TXTID = input("\033[96m ป้อนรหัสข้อความของ SMS \n\t -->>")
+  os.system(f"curl https://textbelt.com/status/{TXTID}")
+  input("\nกด Enter เพื่อออก..")
+  print("\nขอบคุณที่ใช้ SMS..")
+  print("\tเราหวังว่าจะได้พบคุณอีกครั้ง\n พิมพ์ bash Run.sh\n\to Run Again..")
+  print("ขอขอบคุณสำหรับเวลาของคุณ...")
+  exit()
+
+def update():
+    stuff_to_update = ['fakesms.py', 'Run.sh', '.version']
+    for fl in stuff_to_update:
+        dat = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/" + fl).read()
+        file = open(fl, 'wb')
+        file.write(dat)
+        file.close()
+    print('\n\t\tอัพเดทสำเร็จ !!!!')
+    print('\tเรียกใช้สคริปต์อีกครั้ง...')
+    exit()
+
+
+
+clr()
+banner()
+
+try:
+    urllib.request.urlopen('https://www.google.com')
+except Exception:
+    print("เกิดข้อผิดพลาดขณะเชื่อมต่ออินเทอร์เน็ต!!!")
+    print("\tโปรดเชื่อมต่ออินเทอร์เน็ตเพื่อดำเนินการต่อ...\n")
+    input('กำลังออก....\n กด Enter เพื่อออก....')
+    exit()
+print('\tตรวจสอบการปรับปรุง...')
+ver = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/.version").read().decode('utf-8')
+verl = ''
+try:
+    verl = open(".version", 'r').read()
+except Exception:
+    pass
+if ver != verl:
+    print('\n\t\t มีอัพเดท...')
+    print('\tอัปเดต SMS...')
+    update()
+print("ยินดีด้วย")
+print("เวอร์ชันของคุณเป็นเวอร์ชันล่าสุด")
+print('\n\tกำลังเริ่ม SMS...\n')
+try:
+    noti = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/.notify").read().decode('utf-8')
+    if len(noti) > 10:
+        print('\nการแจ้งเตือน : ' + noti + '\n')
+except Exception:
+    pass
+
+
+while True:
+	print("\033[0mbเครื่องมือนี้ใช้เพื่อส่งข้อความที่ไม่ระบุชื่อ")
+	break
+type = 0
+try:
+	if sys.argv[1] == "track":
+		type = 1
+except Exception:
+	type = 0
+if type == 1:
+	print("ติดตามข้อความที่ไม่ระบุชื่อที่คุณส่งโดยใช้เครื่องมือนี้")
+	print()
+	Track()
+elif type == 0:
+	while True:
+		print("ป้อนรายละเอียดของบุคคลที่คุณต้องการส่งข้อความที่ไม่ระบุชื่อ")
+		cc = input("\033[96m\t ใส่รหัสประเทศ (ไม่มี +): ")
+		if '+' in cc:
+		        tc = list(cc)
+		        tc.remove('+')
+		        cc = ''.join(tc)
+		        cc = cc.strip()
+		if len(cc) >= 4 or len(cc) < 1:
+		        print('\n\nรหัสประเทศไม่ถูกต้อง..\n\t\tรหัสประเทศโดยทั่วไปคือ 1-3 หลัก...\n')
+		        continue
+		pn = input("\033[96mใส่หมายเลขโทรศัพท์(ไม่เติม0) : +" + cc + " ")
+		if len(pn) <= 6:
+		        print('\n\nหมายเลขโทรศัพท์ไม่ถูกต้อง..\n')
+		        continue
+		numbe = cc + pn
+		if not numbe.isdigit():
+		            print('\n\nหมายเลขโทรศัพท์ต้องประกอบด้วยตัวเลขเท่านั้น\n')
+		            continue
+		receiver = '+' + numbe
+		text = input("\033[96m ป้อนข้อความที่จะส่ง : ")
+		
+		resp = requests.post('https://textbelt.com/text',{
+			'phone' : receiver,
+			'message' : text ,
+			'key' : 'textbelt'
+		})
+		
+		print(resp.json())
+		print("ขอขอบคุณสำหรับเวลาของคุณ...")
+		break
+		if '"success" : true ' in resp.text:
+		    print("\033[92m ข้อความที่ส่งประสบความสำเร็จ \033[0m")
+		    input('\n\t\tกด Enter เพื่อออก...')
+		    banner()
+		    exit()
+		if '"success" : false ' in resp.text:
+		    print("\033[91m เกิดข้อผิดพลาด")
+		    print("\033[91m ไม่สามารถส่ง SMS! ")
+		    input('\n\t\tกด Enter เพื่อออก...')
+		    banner()
+		    exit()
+		exit() 
